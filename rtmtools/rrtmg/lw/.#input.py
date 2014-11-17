782b3722b84f1661d06437663a04ec1213ffea45
Jack Yu <llacque@gmail.com>
2014-11-17 10:13:00 +0800

    Re-organised rtmtools' package tree and created some __init__.py files
    Right now, there are two subpackages under rtmtools: lblrtm and rrtmg.
    
    lblrtm acutally contains all the stuff that has been done when working
    with AER's lblrtm and Prof. Chou's line-by-line code, the CRD.  At some
    point in the future, it is probably better to split this into two
    subpackages, 'lblrtm' and 'crd'.
    
    	new file:   rtmtools/rtmtools/__init__.py
    	renamed:    rtmtools/rtmtools/aer_flux_calculation.py -> rtmtools/rtmtools/lblrtm/aer_flux_calculation.py
    	renamed:    rtmtools/rtmtools/aer_lnfl.py -> rtmtools/rtmtools/lblrtm/aer_lnfl.py
    	renamed:    rtmtools/rtmtools/aeranalyse.py -> rtmtools/rtmtools/lblrtm/aeranalyse.py
    	renamed:    rtmtools/rtmtools/aerutils.py -> rtmtools/rtmtools/lblrtm/aerutils.py
    	renamed:    rtmtools/rtmtools/atmosphere_profile.py -> rtmtools/rtmtools/lblrtm/atmosphere_profile.py
    	renamed:    rtmtools/rtmtools/create_LBLRTM_input.py -> rtmtools/rtmtools/lblrtm/create_LBLRTM_input.py
    	renamed:    rtmtools/rtmtools/plotting.py -> rtmtools/rtmtools/lblrtm/plotting.py
    	renamed:    rtmtools/rtmtools/tests/.ipynb_checkpoints/atmpro_extra_level_insertion-checkpoint.ipynb -> rtmtools/rtmtools/lblrtm/tests/.ipynb_checkpoints/atmpro_extra_level_insertion-checkpoint.ipynb
    	renamed:    rtmtools/rtmtools/tests/.ipynb_checkpoints/modularise_lbl-checkpoint.ipynb -> rtmtools/rtmtools/lblrtm/tests/.ipynb_checkpoints/modularise_lbl-checkpoint.ipynb
    	renamed:    rtmtools/rtmtools/tests/.ipynb_checkpoints/pandas_HDF5_IO-checkpoint.ipynb -> rtmtools/rtmtools/lblrtm/tests/.ipynb_checkpoints/pandas_HDF5_IO-checkpoint.ipynb
    	renamed:    rtmtools/rtmtools/tests/.ipynb_checkpoints/pandas_multi_index-checkpoint.ipynb -> rtmtools/rtmtools/lblrtm/tests/.ipynb_checkpoints/pandas_multi_index-checkpoint.ipynb
    	renamed:    rtmtools/rtmtools/tests/TAPE5 -> rtmtools/rtmtools/lblrtm/tests/TAPE5
    	renamed:    rtmtools/rtmtools/tests/TAPE5_1altitude -> rtmtools/rtmtools/lblrtm/tests/TAPE5_1altitude
    	renamed:    rtmtools/rtmtools/tests/TAPE5args -> rtmtools/rtmtools/lblrtm/tests/TAPE5args
    	renamed:    rtmtools/rtmtools/tests/atmpro_extra_level_insertion.ipynb -> rtmtools/rtmtools/lblrtm/tests/atmpro_extra_level_insertion.ipynb
    	renamed:    rtmtools/rtmtools/tests/modularise_lbl.ipynb -> rtmtools/rtmtools/lblrtm/tests/modularise_lbl.ipynb
    	renamed:    rtmtools/rtmtools/tests/pandas_HDF5_IO.ipynb -> rtmtools/rtmtools/lblrtm/tests/pandas_HDF5_IO.ipynb
    	renamed:    rtmtools/rtmtools/tests/pandas_multi_index.ipynb -> rtmtools/rtmtools/lblrtm/tests/pandas_multi_index.ipynb
    	renamed:    rtmtools/rtmtools/tests/test_PT2altitudes.py -> rtmtools/rtmtools/lblrtm/tests/test_PT2altitudes.py
    	renamed:    rtmtools/rtmtools/tests/test_atmpro_units_tags_translate.py -> rtmtools/rtmtools/lblrtm/tests/test_atmpro_units_tags_translate.py
    	renamed:    rtmtools/rtmtools/tests/test_create_LBLRTM_input.py -> rtmtools/rtmtools/lblrtm/tests/test_create_LBLRTM_input.py
    	renamed:    rtmtools/rtmtools/tests/test_insert_levels_and_layers.py -> rtmtools/rtmtools/lblrtm/tests/test_insert_levels_and_layers.py
    	new file:   rtmtools/rtmtools/rrtmg/__init__.py
    	new file:   rtmtools/rtmtools/rrtmg/lw/.#input.py
    	new file:   rtmtools/rtmtools/rrtmg/lw/input.py
    	new file:   rtmtools/rtmtools/rrtmg/scripts/rrtmg_lw.py

---
+++ rtmtools/rrtmg/lw/.#input.py
@@ -0,0 +1,1 @@
+jackyu@nuwaln01.nuwa.sinica.edu.tw.11198:1400057530
---
+++ rtmtools/rrtmg/lw/input.py
@@ -0,0 +1,17 @@
+
+
+
+def record_1_1(CXID = ''):
+    return '$' + CXID
+
+
+def record_1_2(IAER = None,
+               IATM = None,
+               IXSECT = None,
+               NUMANGS = None,
+               IOUT = None,
+               IDRV = None,
+               IMCA = None,
+               ICLD = None):
+    return '{}'
+    
