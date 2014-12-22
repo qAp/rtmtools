1d5170614f839666beffc57757d1d67fb85cc706
Jack Yu <llacque@gmail.com>
2014-12-22 16:17:25 +0800

    CLIRAD-SW: Reads bandwise output of fluxes and heating rate into Pandas Panel

--- rtmtools/clirad/sw/wrangle.py
+++ rtmtools/clirad/sw/wrangle.py
@@ -29,4 +29,45 @@ def output_txtfile_to_DataFrame(readfrom = './tt-output-now.dat'):
                       sort_index(ascending = False)
     
 
+
+def OUTPUT_CLIRAD_to_PandasPanel(readfrom = 'OUTPUT_CLIRAD.dat',
+                                 cooling_rate = False,
+                                 signed_fluxes = False):
+    '''
+    Reads output data from CLIRAD into a Pandas Panel of dimensions
+    (wavenumber bands, pressure, [flux up, flux down, net flux, heating rate])
+    '''
+    with open(readfrom, mode = 'r', encoding = 'utf-8') as f:
+        c = f.read()
+
+    content_wbs = (s.strip() for s in c.split('WAVENUMBER BAND:')\
+                   if s and not s.isspace())
+
+    data = {}
+    for content_wb in content_wbs:
+        id_wb = int(content_wb.split(maxsplit = 1)[0])
+        df_wb = pd.read_csv(io.StringIO(content_wb), \
+                skiprows = 3, header = None, index_col = [0], \
+                sep = r'\s+')
+        df_wb.index.name = None #this removes a '0' name for dataframe index
+        data[id_wb] = df_wb
+
+    pnl = pd.Panel(data)
+
+    if cooling_rate:
+        pnl.values[:, :, -1] *= -1
+        rate_label = 'cooling_rate'
+    else:
+        rate_label = 'heating_rate'
+
+    if signed_fluxes:
+        pnl.values[:, :, 1] *= -1
+        pnl.values[:, :, 3] = pnl.values[:, :, 1] + pnl.values[:, :, 2]
+    
+    pnl.minor_axis = ['pressure', 'flux_up', 'flux_down', 'net_flux', rate_label]
+    return pnl
+        
+        
+
+
     
