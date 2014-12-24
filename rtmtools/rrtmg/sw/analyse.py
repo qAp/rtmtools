7a329467eacd3b57227a65cfde7c996e0a5f3f95
Jack Yu <llacque@gmail.com>
2014-12-24 17:14:29 +0800

    Add function to sum over wavenumbers OUTPUT_RRTM(SW)

---
+++ rtmtools/rrtmg/sw/analyse.py
@@ -0,0 +1,33 @@
+import os
+import collections
+import itertools
+import io
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+
+
+def sum_OUTPUT_RRTM_over_wbands(pnl,
+                                names = ['flux_up', 'flux_down', 'net_flux',
+                                         'heating_rate'],
+                                V1 = 820., V2 = 50000.):
+    '''
+    Sum fluxes and/or rates (heating or cooling) over wavenumbers
+    INPUT:
+    pnl --- Pandas Panel: ((V1, V2), atm level, [pressure, flux up, ..., rate])
+    names --- attributes to sum up over wavenumbers
+    V1, V2 --- lower and upper wavenumber limits in the sum
+    OUTPUT:
+    df --- Pandas DataFrame: (atm level, NAMES)
+    '''
+    V1s, V2s = (lev.values for lev in pnl.items.levels)
+    item1, item2 = (np.abs(vs - v).argmin() 
+                    for vs, v in zip([V1s, V2s], [V1, V2]))
+    return pd.concat([pnl.ix[pnl.items[0], :, 'pressure'],
+                      pnl.ix[item1: item2, :, names].sum(axis = 'items')],
+                     axis = 1)
