97f4ac91b54da23b53f0e3e4d3ba731a7ca94b2f
Jack Yu <llacque@gmail.com>
2014-12-24 17:09:40 +0800

    Add function to convert OUTPUT_RRTM (SW) to Pandas Panel

--- rtmtools/rrtmg/sw/wrangle.py
+++ rtmtools/rrtmg/sw/wrangle.py
@@ -28,3 +28,47 @@ def output_txtfile_to_DataFrame(readfrom = './zz-output-onlysw-now.txt'):
     data = np.array([toprow] + restrows).astype('f8')
     return pd.DataFrame(data = data[:: -1], columns = columns).\
                       sort_index(ascending = False)
+
+
+def OUTPUT_RRTM_to_pandasPanel(readfrom = '', cooling_rate = False,
+                               signed_fluxes = False):
+
+    '''
+    Read and convert OUTPUT_RRTM file from RRTMG-SW to Pandas
+    Panel object
+    '''
+    with open(readfrom, mode = 'r', encoding = 'utf-8') as file:
+        c = file.read()
+
+    content_wbs = [s.strip()
+                   for s in c.split('\x0c')[1: -1] if s and not s.isspace()]
+
+    data = {}
+    for content_wb in content_wbs:
+        l1 = content_wb.split('\n', maxsplit = 1)[0].split()
+        V1, V2 = float(l1[1]), float(l1[3])
+        df = pd.read_csv(io.StringIO(content_wb),
+                         header = None, skiprows = 3, index_col = [0],
+                         sep = r'\s+')
+        df.index.name = None
+        data[(V1, V2)] = df
+
+    pnl = pd.Panel(data)
+
+    if cooling_rate:
+        pnl.values[:, :, -1] *= -1
+        rate_label = 'cooling_rate'
+    else:
+        rate_label = 'heating_rate'
+
+    if signed_fluxes:
+        pnl.values[:, :, 1] *= -1
+        pnl.values[:, :, 5] = pnl.values[:, :, 1] + pnl.values[:, :, 4]
+
+    pnl.minor_axis = ['pressure', 'flux_up',
+                      'flux_difdown', 'flux_dirdown',
+                      'flux_down', 'net_flux', rate_label]
+    return pnl
+        
+    
+        
